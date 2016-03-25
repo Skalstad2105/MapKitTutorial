98db956ca91c528ec78070878efcd45f17d17aec
Marius Skalstad <Skalstad10@gmail.com>
2016-03-25 08:47:22 +0100

    Initial Commit

---
+++ MapKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapKit
+//
+//  Created by Cato Skalstad on 25.03.2016.
+//  Copyright © 2016 Marius Skalstad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
